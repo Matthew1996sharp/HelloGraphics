421b40ae5b762a23dfd3fb55461e0198fcd9c4ee
matvei <matvei@tereschenko.info>
2022-05-30 14:19:12 +0300

    Добавьте файлы проекта.

---
+++ WinForm.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace HelloGraphics
+{
+    partial class WinForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // WinForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 161);
+            this.Name = "WinForm";
+            this.Text = "Приветствие";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.WinForm_Paint);
+            this.Resize += new System.EventHandler(this.WinForm_Resize);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ WinForm.cs
@@ -0,0 +1,59 @@
+﻿// Программа демонстрирует вывод текста на поверхность формы 
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloGraphics
+{
+    public partial class WinForm : Form
+    {
+        public WinForm()
+        {
+            InitializeComponent();
+        }
+
+        //обработка события Paint
+        private void WinForm_Paint(object sender, PaintEventArgs e)
+        {
+            //вывод текста шрифтом, заданным значением свойства Font формы
+            e.Graphics.DrawString("Microsoft Visual C#", this.Font, Brushes.DarkGreen, 10, 10);
+
+            //вывод текста шрифтом, созданным программистом
+            Font aFont = new Font("Tahoma", 12, FontStyle.Regular);
+            e.Graphics.DrawString("Microsoft Visual C#", aFont, Brushes.Black, 10, 30);
+
+            //вывод текста в центре формы
+            Font hFont = new Font("Tahoma", 14, FontStyle.Underline);
+            string header = "Microsoft Visual Studio C# 2022 Community Edition";
+
+            //размер области отображения текста зависит от
+            //характеристик шрифта, которым он отображается
+
+            //определить размер области отображения текста
+            int width = (int)e.Graphics.MeasureString(header, hFont).Width;
+            int height = (int)e.Graphics.MeasureString(header, hFont).Height;
+
+            //вычислить координаты левого верхнего угла области отображения текста,
+            //так чтобы текст был размещен в центре формы по горизонтали и вертикали.
+            //ClientSize.Width - размер внутренней области формы
+            int x = (this.ClientSize.Width - width) / 2;
+            int y = (this.ClientSize.Height - height) / 2;
+
+            //вывести текст в центре формы
+            e.Graphics.DrawString(header, hFont, Brushes.DarkGreen, x, y);
+            e.Graphics.DrawString(header, hFont, Brushes.DarkGreen, x, y + height);
+        }
+
+        private void WinForm_Resize(object sender, EventArgs e)
+        {
+            this.Refresh();
+        }
+    }
+}
